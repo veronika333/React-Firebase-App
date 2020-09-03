1199d9ceeb5b6a7ecffb063f721ed78f254aba66
veronika333 <vera3@inbox.ru>
2020-09-03 09:52:27 +0300

    style and toggling added to login

--- src/components/Auth/login/Login.js
+++ src/components/Auth/login/Login.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState } from 'react';
 import './login.css';
 
 export default function Login() {
+const [login, setLogin] = useState(true);
+
+
     return (
         <div className="login-container">
-            <h2>Create Account</h2>
+            <h2>{login ? "Login" : "Create Account"}</h2>
             <form>
-                <input type="text" placeholder="Your name" autoComplete="off" />
+                {!login && (<input type="text" placeholder="Your name" autoComplete="off" />)}
                 <input type="email" placeholder="Your email" autoComplete="off" />
                 <input type="password" placeholder="Type a secure password" />
-           <button type="submit">Register</button>
-           <button type="button">
-Already have an account? Log in
+           <button type="submit">Submit</button>
+           <button type="button" onClick={() => setLogin(prevLogin => !prevLogin)}>
+{!login ? "Or sign in" : "Or create account"}
            </button>
             </form>
         </div>
